975845948e128f57bc8cdbbff08359db6a635ec3
habilithm97 <habinkim97@gmail.com>
2024-07-05 23:27:43 +0900

    안드로이드 스튜디오 버전 업데이트

--- app/src/main/java/com/example/study2407/MainActivity.kt
+++ app/src/main/java/com/example/study2407/MainActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.study2407.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        enableEdgeToEdge() // 전체 화면 사용(시스템바 투명하게)
+
+        setContentView(binding.root)
+
+        // 시스템바의 인셋을 뷰의 패딩으로 설정 -> 뷰의 콘텐츠가 시스템바에 가려지지 않음(UI 가시성 보장, UX 향상)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
